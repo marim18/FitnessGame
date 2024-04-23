a59815f8bfaeabdf1c2579c183e0ecd4202e9b19
Maria Møll <marim18@uia.no>
2024-04-23 16:46:21 +0200

    Progressing on genereal plan of the program

--- Game.cs
+++ Game.cs
@@ -3,7 +3,7 @@ using System.Threading;
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 using SecretLabs.NETMF.Hardware;
-using SecretLabs.NETMF.Hardware.Netduino;
+using SecretLabs.NETMF.Hardware.Netduino; //idk which of these we actually need they were in a template i found
 
 
 
@@ -16,11 +16,25 @@ void setup()
 {
     pinMode(redPin, OUTPUT);
     pinMode(greenPin, OUTPUT);
-    pinMode;
+    pinMode(bluePin,OUTPUT);
     Serial.begin(115200); //idk what this does
 }
 void Program (){
-    
-
-
+    while (numberoflights < count)
+    {
+        int random = new RandomNumberGenerator;
+        sequence.Add(random);
+    }
+}
+void Lightup(int LightID)
+{
+    analogWrite(redPin, 255);
+    Timer timer = new timer;
+    timer.activate();
+}
+void buttonPressed()
+{
+    //do stuff
+    int value = timer;
+    timer.stop();
 }

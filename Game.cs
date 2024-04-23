a3eb05e701dec4c74b6e48efa86f21542e542b3c
Maria Møll <marim18@uia.no>
2024-04-23 16:23:58 +0200

    First version of setup

--- Game.cs
+++ Game.cs
@@ -1,1 +1,26 @@
-//Hello this is where i work now
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+
+
+int redPin = 17;
+int greenPin = 16;
+int bluePin = 5;
+int ButtonPin = 18;
+
+void setup()
+{
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode;
+    Serial.begin(115200); //idk what this does
+}
+void Program (){
+    
+
+
+}

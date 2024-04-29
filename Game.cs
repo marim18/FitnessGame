b681106020c58fada42e449ef600263966a5c92d
marim18 <97792648+marim18@users.noreply.github.com>
2024-04-29 20:23:19 +0200

    Added additional version file this functions as attempt archive

--- Game.cs
+++ Game.cs
@@ -5,7 +5,7 @@ using Microsoft.SPOT.Hardware;
 using SecretLabs.NETMF.Hardware;
 using SecretLabs.NETMF.Hardware.Netduino; //idk which of these we actually need they were in a template i found
 
-
+//com 8
 
 int redPin = 17;
 int greenPin = 16;
@@ -26,15 +26,32 @@ void Program (){
         sequence.Add(random);
     }
 }
-void Lightup(int LightID)
-{
-    analogWrite(redPin, 255);
-    Timer timer = new timer;
-    timer.activate();
-}
+
 void buttonPressed()
 {
     //do stuff
     int value = timer;
     timer.stop();
+}
+
+class lights
+{
+    int redPin;
+    int greenPin;
+    int bluePin;
+    int id;
+
+    Light1 = new lights{
+    this.ID = 1;
+    this.redPin = 17;
+    this.greenPin = 16;
+    this.bluePin = 5;
+    this.ButtonPin = 18; }
+
+    void Lightup(lights light)
+    {
+        analogWrite(redPin, 255);
+        Timer timer = new timer;
+        timer.activate();
+    }
 }
